8bb68cf530bfc8952eec7c4819a7c83ca25ede36
sboikov <sboikov@gridgain.com>
2016-02-17 11:35:01 +0300

    ignite-1232 Temporary workaround for case when affinity key is not sql field

--- modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/query/GridQueryProcessor.java
@@ -289,6 +289,12 @@ public class GridQueryProcessor extends GridProcessorAdapter {
                         altTypeId = new TypeId(ccfg.getName(), ctx.cacheObjects().typeId(qryEntity.getValueType()));
                     }
 
+                    if (desc.affinityKey() != null) {
+                        // TODO: IGNITE-1232
+                        if (!desc.fields().containsKey(desc.affinityKey()))
+                            desc.affinityKey(null);
+                    }
+
                     addTypeByName(ccfg, desc);
                     types.put(typeId, desc);
 
